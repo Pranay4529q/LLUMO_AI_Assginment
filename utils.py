bdc2d3937b7aea3928c7c5cc2852e340760b5f86
Pranay4529q <bollikondapranay8@gmail.com>
2025-09-13 19:44:12 +0530

    update utils,py

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from datetime import datetime, date
 from typing import Dict, Any
-
+"my main aim is :"
+"remove the _id and convert the joining_date from objects  to string"
+""
 def serialize_employee(doc: Dict[str, Any]) -> Dict[str, Any]:
 
     if not doc:
